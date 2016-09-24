21f9682a823fa4dbb6dd96e579ac3f36e351509d
Terry Jean <tjean019@fiu.edu>
2016-09-24 18:18:13 -0400

    Added InventoryType Class

---
+++ HotelsManagement/InventoryType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelsManagement
+{
+    public class InventoryType
+    {
+        public String Date { get; set; }
+        public int HotelId { get; set; }
+        public Room.BedType RoomType { get; set; }
+        public int Quantity { get; set; }
+
+        public InventoryType() { }
+
+        public InventoryType(int hotelid, DateTime date, Room.BedType bedtype = Room.BedType.DOUBLE, int quantity = 0)
+        {
+            this.Date = date.ToShortDateString();
+            this.HotelId = hotelid;
+            this.RoomType = bedtype;
+            this.Quantity = quantity;
+        }
+
+    }
+}
