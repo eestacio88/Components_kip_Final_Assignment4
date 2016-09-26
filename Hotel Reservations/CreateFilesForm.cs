c23f388925f58e23713a5a178e1cb6f8b969720e
Terry Jean <terrymiami2@aol.com>
2016-09-25 20:18:21 -0400

    Update Bedtype enum names.

--- Hotel Reservations/CreateFilesForm.cs
+++ Hotel Reservations/CreateFilesForm.cs
@@ -71,16 +71,16 @@ namespace Hotel_Reservations
                 hotel.Features.Distances.Beach = Math.Round(r.NextDouble() * 5, 2);
                 hotel.Features.Distances.Shopping = Math.Round(r.NextDouble() * 5, 2);
            
-                Room KB = new Room(Room.BedType.KING, r.Next(90 + 1) + 100, 5);
+                Room KB = new Room(Room.BedType.KB, r.Next(90 + 1) + 100, 5);
                 hotel.RoomList.Add(KB);
 
-                Room QB = new Room(Room.BedType.QUEEN, r.Next(90 + 1) + 100, 4);
+                Room QB = new Room(Room.BedType.QB, r.Next(90 + 1) + 100, 4);
                 hotel.RoomList.Add(QB);
 
-                Room DB = new Room(Room.BedType.DOUBLE, r.Next(90 + 1) + 100, 2);
+                Room DB = new Room(Room.BedType.DB, r.Next(90 + 1) + 100, 2);
                 hotel.RoomList.Add(DB);
 
-                Room BS = new Room(Room.BedType.SUITE, r.Next(90 + 1) + 100, 6);
+                Room BS = new Room(Room.BedType.BS, r.Next(90 + 1) + 100, 6);
                 hotel.RoomList.Add(BS);
 
                 this.hotelManager.hotels.Add(hotel);
